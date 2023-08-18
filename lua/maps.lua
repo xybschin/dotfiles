fa29600d4f54a90c369607dcdfb886095759c755
Bjarne Schindler <bjarne.schindler@bela.de>
2023-08-18 12:06:23 +0200

    update lazygit keybind

--- lua/maps.lua
+++ lua/maps.lua
@@ -2,4 +2,4 @@ local keymap = vim.keymap
 
 keymap.set("n", "<Space>tf", ":NvimTreeFindFile<Return>", { noremap = true, silent = true })
 keymap.set("n", "<Space>tt", ":NvimTreeFocus<Return>", { noremap = true, silent = true })
-keymap.set("n", "lg", ":FloatermNew lazygit<Return>")
+keymap.set("n", "lg", ":FloatermNew --height=0.9 --height=0.9 --wintype='float' lazygit<Return>")
