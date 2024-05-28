3053ed98148f740840e2cc026603aae1c454be77
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-05-28 21:18:55 +0200

    add telescop and trouble

--- lua/bjarne/core/keymaps.lua
+++ lua/bjarne/core/keymaps.lua
@@ -1,8 +1,5 @@
 local opt = { silent = true, noremap = true }
 
-vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>qa<cr>", opt)
-vim.api.nvim_set_keymap("n", "<leader>x", "<cmd>x!<cr>", opt)
-vim.api.nvim_set_keymap("i", "<c-s>", "<esc><cmd>w<cr>a", opt)
 vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>w<cr>", opt)
 vim.api.nvim_set_keymap("n", "<a-h>", "<c-w>h", opt)
 vim.api.nvim_set_keymap("n", "<a-j>", "<c-w>j", opt)
--- lua/bjarne/plugins/init.lua
+++ lua/bjarne/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-  { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = "nvim-lua/plenary.nvim" },
   { "akinsho/toggleterm.nvim", version = "2.11.0", config = true },
   { "zbirenbaum/copilot.lua", cmd = "Copilot", event = "InsertEnter" },
   { "rafamadriz/friendly-snippets" },
