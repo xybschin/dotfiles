8702888e88abe2bbf138c3d06b1a16bd898a7322
Bjarne Schindler <bjarne.schindler@adesso.de>
2024-05-29 11:57:27 +0200

    add more configuration files

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local config = {}
+local wezterm = require("wezterm")
+
+local is_windows = package.config:sub(1, 1) == "\\"
+if is_windows then
+	config.default_prog = { "powershell.exe", "-NoLogo" }
+end
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+config.color_scheme = "Batman"
+config.colors = { background = "#121212" }
+
+config.font_size = 11
+config.font = wezterm.font({
+	family = "Iosevka Nerd Font",
+	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+})
+
+return config
