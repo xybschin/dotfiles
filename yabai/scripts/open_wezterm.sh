7c32336cf7e7d9d709dd4c62ddcf913fc2923cf8
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-07-08 20:09:02 +0200

    update wezterm script

--- yabai/scripts/open_wezterm.sh
+++ yabai/scripts/open_wezterm.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 
-if ! pgrep -f "wezterm" > /dev/null 2>&1; then
-    open -a "/Applications/WezTerm.app"
-else
-    script='tell application "WezTerm" to create window with default profile'
-    ! osascript -e "${script}" > /dev/null 2>&1 && {
-        while IFS="" read -r pid; do
-            kill -15 "${pid}"
-        done < <(pgrep -f "wezterm")
-        open -a "/Applications/WezTerm.app"
-    }
-fi
+open -n "/Applications/WezTerm.app"
