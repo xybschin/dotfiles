6c9ac8cef705b12aa68331fde0a0a3cddca0c8f8
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-08-31 22:35:30 +0200

    use rose pine as lualine theme

--- nvim/lua/bjarne/plugins/lualine.lua
+++ nvim/lua/bjarne/plugins/lualine.lua
@@ -1,7 +1,15 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    options = { theme = "gruvbox" },
-  },
+  config = function()
+    local theme = require "lualine.themes.rose-pine"
+    local modes = { "normal", "insert", "visual", "replace", "command", "inactive" }
+    for _, mode in ipairs(modes) do
+      theme[mode].c.bg = "NONE"
+    end
+
+    require("lualine").setup {
+      options = { theme = theme },
+    }
+  end,
 }
