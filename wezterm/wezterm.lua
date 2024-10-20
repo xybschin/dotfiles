dee0465a14d3298bd1b5a24e00baef06ab28c82e
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-10-20 10:01:59 +0200

    update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,19 @@
-local config = {}
 local wezterm = require("wezterm")
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
 
-local is_windows = package.config:sub(1, 1) == "\\"
-if is_windows then
-	config.default_prog = { "wsl" }
-end
+custom.background = "#000000"
+custom.tab_bar.background = "#040404"
+custom.tab_bar.inactive_tab.bg_color = "#0f0f0f"
+custom.tab_bar.new_tab.bg_color = "#080808"
 
-config.window_decorations = "RESIZE"
-config.hide_tab_bar_if_only_one_tab = true
-
-local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
-
-config.colors = theme.colors()
-config.font_size = 16
-config.colors["background"] = "#000"
-config.font = wezterm.font({
-	family = "JetBrainsMono Nerd Font",
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-})
-
-return config
+return {
+	window_decorations = "RESIZE",
+	hide_tab_bar_if_only_one_tab = true,
+	font_size = 16,
+	font = wezterm.font({
+		family = "JetBrainsMono Nerd Font",
+		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+	}),
+	color_schemes = { ["OLEDppuccin"] = custom },
+	color_scheme = "OLEDppuccin",
+}
