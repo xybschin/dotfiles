05732c1fe70dfb1d98dd6a77221e9bd76a9d7069
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-11-04 19:34:00 +0100

    update

--- nvim/.config/nvim/lua/bjarne/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/bjarne/plugins/neo-tree.lua
@@ -8,7 +8,6 @@ return {
     "mrbjarksen/neo-tree-diagnostics.nvim",
   },
   opts = {
-    close_if_last_window = true,
     popup_border_style = "rounded",
     enable_git_status = true,
     enable_diagnostics = true,
