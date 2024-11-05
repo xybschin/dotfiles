94660ec5346608330ba5de10794312901d789be9
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-11-05 19:54:32 +0100

    update

--- nvim/.config/nvim/lua/bjarne/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/bjarne/plugins/neo-tree.lua
@@ -1,3 +1,8 @@
+vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
+
 return {
   "nvim-neo-tree/neo-tree.nvim",
   branch = "v3.x",
@@ -59,11 +64,6 @@ return {
     },
   },
   init = function()
-    vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-    vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-    vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-    vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-
     vim.keymap.set("n", "<C-b>", "<cmd>Neotree toggle last<cr>")
   end,
 }
