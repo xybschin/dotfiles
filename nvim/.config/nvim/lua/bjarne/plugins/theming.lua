52834fef2e404ebf4132a69d8d4e9ddfc57b74ab
Bjarne Schindler <bjarne.schindler@student.fh-kiel.de>
2024-11-07 20:01:06 +0100

    update

--- nvim/.config/nvim/lua/bjarne/plugins/theming.lua
+++ nvim/.config/nvim/lua/bjarne/plugins/theming.lua
@@ -1,17 +1,24 @@
 return {
   { "lewis6991/gitsigns.nvim" },
-  {
-    "savq/melange-nvim",
-    init = function()
-      vim.cmd.colorscheme "melange" -- colorscheme still needs vim.cmd
-    end,
-  },
-  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
   -- {
-  --   "rose-pine/neovim",
-  --   name = "rose-pine",
+  --   "savq/melange-nvim",
   --   init = function()
-  --     vim.cmd.colorscheme "rose-pine"
+  --     vim.cmd.colorscheme "melange" -- colorscheme still needs vim.cmd
   --   end,
   -- },
+  { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+  {
+    "rose-pine/neovim",
+    opts = {
+      palette = {
+        main = {
+          base = "#0D0C16",
+        },
+      },
+    },
+    name = "rose-pine",
+    init = function()
+      vim.cmd.colorscheme "rose-pine"
+    end,
+  },
 }
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,8 @@
 local wezterm = require("wezterm")
+local theme = wezterm.plugin.require("https://github.com/neapsix/wezterm").main
+
+local colors = theme.colors()
+colors["background"] = "#0D0C14"
 
 return {
 	window_decorations = "RESIZE",
@@ -8,5 +12,6 @@ return {
 		family = "JetBrainsMono Nerd Font",
 		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	}),
-	color_scheme = "melange_dark",
+	colors = colors,
+	-- color_scheme = "melange_dark",
 }
