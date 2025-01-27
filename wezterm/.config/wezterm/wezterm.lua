bb9850d51518446c04f4d5eebc00e8c9b86388c8
Bjarne Schindler <bjarne.schindler.ext@zeiss.com>
2025-01-27 13:55:33 +0100

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,9 +3,9 @@ local wezterm = require("wezterm")
 return {
 	window_decorations = "RESIZE",
 	hide_tab_bar_if_only_one_tab = true,
-	font_size = 15,
+	font_size = 11,
 	font = wezterm.font({
-		family = "JetBrainsMono Nerd Font",
+		family = "Iosevka Nerd Font",
 		harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
 	}),
 	color_scheme = "alabaster_dark",
