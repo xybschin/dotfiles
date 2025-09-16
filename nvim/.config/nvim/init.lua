c6471493746de890e9510c03cb7cef135929ef1e
xybschin <hello@bjarneschindler.dev>
2025-09-16 20:27:44 +0200

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -961,6 +961,7 @@ require('lazy').setup({
 
       require('mini.jump').setup()
       require('mini.align').setup()
+      require('mini.tabline').setup()
       require('mini.pairs').setup()
       require('mini.snippets').setup()
     end,
@@ -1001,8 +1002,8 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
