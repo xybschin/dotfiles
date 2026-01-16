57cab45414e752965fd83b01a3694a9fa78c20e2
xybschin <hello@bjarneschindler.dev>
2026-01-16 14:21:03 +0100

    update

--- hypr/.config/hypr/scripts/winetray.sh
+++ hypr/.config/hypr/scripts/winetray.sh
@@ -4,13 +4,20 @@ SOCK="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
 
 socat -U - UNIX-CONNECT:$SOCK | while read -r line; do
     [[ "$line" =~ ^openwindow ]] || continue
+    echo "Window opened: $line"
     IFS=',' read -r addr _ class <<< "${line#openwindow>>}"
     wininfo=$(hyprctl clients -j | jq -r ".[] | select(.address==\"0x$addr\")")
+
     [[ -z "$wininfo" ]] && continue
+    echo "wininfo: $wininfo"
+
     width=$(echo "$wininfo" | jq '.size[0]')
     height=$(echo "$wininfo" | jq '.size[1]')
     winclass=$(echo "$wininfo" | jq -r '.class')
+
+
     if [[ "$winclass" == "steam_app_default" && "$width" -eq 160 && "$height" -eq 20 ]]; then
+        echo "moving window silently out of your way"
         hyprctl dispatch movetoworkspacesilent special
     fi
 done
