0db09f93ab73cc252544f5ca8e116bdc0e52b76a
xybschin <hello@bjarneschindler.dev>
2026-01-23 11:48:12 +0100

    update

---
+++ hypr/.config/hypr/scripts/hidewinetray.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+SOCK="$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock"
+
+socat -U - UNIX-CONNECT:$SOCK | while read -r line; do
+  [[ "$line" =~ ^openwindow ]] || continue
+  IFS=',' read -r addr _ class <<< "${line#openwindow>>}"
+  wininfo=$(hyprctl clients -j | jq -r ".[] | select(.address==\"0x$addr\")")
+  [[ -z "$wininfo" ]] && continue
+  width=$(echo "$wininfo" | jq '.size[0]')
+  height=$(echo "$wininfo" | jq '.size[1]')
+  winclass=$(echo "$wininfo" | jq -r '.class')
+  if [[ "$winclass" == "steam_app_default" && "$width" -eq 160 && "$height" -eq 20 ]]; then
+    notify-send "Detected a tray icon from wine, moving it to workspace 'special'"
+    hyprctl dispatch movetoworkspacesilent special
+  fi
+done
+
