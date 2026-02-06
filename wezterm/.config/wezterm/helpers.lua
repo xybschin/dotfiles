94839de390cfbe2aacb39051aae992ee49c24896
xybschin <hello@bjarneschindler.dev>
2026-02-06 08:28:04 +0100

    update

--- wezterm/.config/wezterm/helpers.lua
+++ wezterm/.config/wezterm/helpers.lua
@@ -2,15 +2,18 @@ local wezterm = require("wezterm")
 local module = {}
 
 local function is_unix()
-  return package.config:sub(1,1) == '/'
+	return package.config:sub(1, 1) == "/"
 end
 
 function module.apply_platform_settings(config)
-  -- set settings only when on windows
-  if not is_unix() then
-    config.default_prog = { 'powershell.exe', '-NoLogo' }
-    config.default_domain = 'WSL:FedoraLinux-43'
-  end
+	-- set settings only when on windows
+	if not is_unix() then
+		config.default_prog = { "powershell.exe", "-NoLogo" }
+		local default_domains = wezterm.default_wsl_domains()
+		if #default_domains > 1 then
+			config.default_domain = default_domains[1]
+		end
+	end
 end
 
 return module
