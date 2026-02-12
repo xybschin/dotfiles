644157918c579025488c2bcdb54f9b5e7aeb439a
xybschin <hello@bjarneschindler.dev>
2026-02-12 15:49:09 +0100

    updater

--- nvim/.config/nvim/plugin/30_mini.lua
+++ nvim/.config/nvim/plugin/30_mini.lua
@@ -625,7 +625,18 @@ end)
 -- Example usage in Visual mode:
 -- - `<M-h>`/`<M-j>`/`<M-k>`/`<M-l>` - move selection left/down/up/right
 later(function()
-	require("mini.move").setup()
+	require("mini.move").setup({
+		mappings = {
+			left = "<C-h>",
+			right = "<C-l>",
+			down = "<C-j>",
+			up = "<C-k>",
+			line_left = "<C-h>",
+			line_right = "<C-l>",
+			line_down = "<C-j>",
+			line_up = "<C-k>",
+		},
+	})
 end)
 
 -- Text edit operators. All operators have mappings for:
